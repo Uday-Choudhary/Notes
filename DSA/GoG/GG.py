2c959327de67f4f91c2e06924e1255cb89a3fe4b
Uday-Choudhary <choudharyuday85@gmail.com>
2025-06-22 17:28:51 +0530

    Resolved conflicts

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -1,5 +1,18 @@
-# # User function Template for python3
+# Day1
+# class Solution:
+#     def getSecondLargest(self, arr):
+#         first = second = -1
 
+#         for num in arr:
+#             if num > first:
+#                 second = first
+#                 first = num
+#             elif num != first and num > second:
+#                 second = num
+
+#         return second
+
+# Day2
 # class Solution:
 # 	def pushZerosToEnd(self, arr):
 #         pos = 0
