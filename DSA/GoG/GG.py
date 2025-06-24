2ffd8c7de23c0f881aa8a78410efb0a3fdfa2d2b
Uday-Choudhary <choudharyuday85@gmail.com>
2025-06-24 09:09:34 +0530

    day 4

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -26,18 +26,39 @@
 
 #         return arr
 
-Day3
+# Day3
+# class Solution:
+#     def reverseArray(self, arr):
+#         i = 0
+#         j = len(arr) - 1
+#         temp = -1
+#         while (i <= j):
+#             temp = arr[i]
+#             arr[i] = arr[j]
+#             arr[j] = temp
+#             i += 1
+#             j -= 1
+#         return arr
 
+# Day4
+# User function Template for python3
 
 class Solution:
-    def reverseArray(self, arr):
-        i = 0
-        j = len(arr) - 1
-        temp = -1
-        while (i <= j):
-            temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
-            i += 1
-            j -= 1
+    # Function to rotate an array by d elements in counter-clockwise direction.
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        d = d % n
+        pos = 0
+        stored_part = []
+        for i in range(d):
+            stored_part.append(arr[i])
+
+        for i in range(d, n):
+            arr[pos] = arr[i]
+            pos += 1
+
+        for i in range(d):
+            arr[pos] = stored_part[i]
+            pos += 1
+
         return arr
