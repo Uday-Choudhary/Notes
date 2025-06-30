d134c99e3f366c90bba8aa72a5e949a5e1067fcf
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-06-30 22:47:40 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -96,3 +96,12 @@ class Solution:
                 continue
             min_diff = min(min_diff , max_elem - min_elem)
         return min_diff
+# Day10
+class Solution:
+    def maxSubArraySum(self, arr):
+        max_current = max_global = arr[0]
+        
+        for i in range(1 , len(arr)):
+            max_current = max(arr[i] , max_current + arr[i])
+            max_global = max(max_global , max_current)
+        return max_global
