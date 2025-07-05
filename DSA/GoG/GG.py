cad65c974def43fafee1c1c6061cb0f01d6da84f
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-05 22:35:51 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -190,4 +190,28 @@ class Solution:
             num = num * 10 + digit
             i+=1
         return sign * num
-        
+
+#Day15 
+#User function Template for python3
+class Solution:
+	def addBinary(self, s1, s2):
+		# code here
+		i,j = len(s1) - 1 , len(s2) - 1
+		carry = 0
+		result = []
+		
+		while i>= 0 or j >=0 or carry:
+		    bit1 = int(s1[i]) if i>= 0 else 0
+		    bit2 = int(s2[j]) if j >= 0 else 0
+		    total = bit1 + bit2 + carry
+		    
+		    result.append(str(total % 2))
+		    carry = total // 2
+		    
+		    i-=1
+		    j-=1
+		    
+		    
+	    result_str = ''.join(reversed(result)).lstrip('0')
+	    
+	    return result_str if result_str else '0'
