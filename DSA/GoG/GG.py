0c7c8e5d01736251ce70ed7e62dea7703ce1ecef
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-09 22:10:16 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -193,28 +193,28 @@ class Solution:
 
 #Day15 
 #User function Template for python3
-class Solution:
-	def addBinary(self, s1, s2):
-		# code here
-		i,j = len(s1) - 1 , len(s2) - 1
-		carry = 0
-		result = []
+# class Solution:
+# 	def addBinary(self, s1, s2):
+# 		# code here
+# 		i,j = len(s1) - 1 , len(s2) - 1
+# 		carry = 0
+# 		result = []
 		
-		while i>= 0 or j >=0 or carry:
-		    bit1 = int(s1[i]) if i>= 0 else 0
-		    bit2 = int(s2[j]) if j >= 0 else 0
-		    total = bit1 + bit2 + carry
+# 		while i>= 0 or j >=0 or carry:
+# 		    bit1 = int(s1[i]) if i>= 0 else 0
+# 		    bit2 = int(s2[j]) if j >= 0 else 0
+# 		    total = bit1 + bit2 + carry
 		    
-		    result.append(str(total % 2))
-		    carry = total // 2
+# 		    result.append(str(total % 2))
+# 		    carry = total // 2
 		    
-		    i-=1
-		    j-=1
+# 		    i-=1
+# 		    j-=1
 		    
 		    
-	    result_str = ''.join(reversed(result)).lstrip('0')
+# 	    result_str = ''.join(reversed(result)).lstrip('0')
 	    
-	    return result_str if result_str else '0'
+# 	    return result_str if result_str else '0'
 # Day16
 # class Solution:
 #     def areAnagrams(self, s1, s2):
