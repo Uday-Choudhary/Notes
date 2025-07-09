4d082e367c5e9889188baa742a410423c0859516
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-09 22:10:45 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -172,24 +172,24 @@ class Solution:
         return n+1
 #Day14 Strings
 #User function template for Python
-class Solution:
-    def myAtoi(self, s):
-        i,n,sign,num = 0 , len(s), 1,0
-        INT_MAX , INT_MIN = 2**31 -1 , -2 **31
+# class Solution:
+#     def myAtoi(self, s):
+#         i,n,sign,num = 0 , len(s), 1,0
+#         INT_MAX , INT_MIN = 2**31 -1 , -2 **31
         
-        while i < n and s[i] == ' ' :
-            i+=1
-        if i < n and s[i] in '+-':
-            sign = -1 if s[i] == '-' else 1
-            i+=1
+#         while i < n and s[i] == ' ' :
+#             i+=1
+#         if i < n and s[i] in '+-':
+#             sign = -1 if s[i] == '-' else 1
+#             i+=1
             
-        while i<n and '0' <= s[i] <= '9':
-            digit = ord(s[i]) - ord('0')
-            if num > (INT_MAX - digit) // 10:
-                return INT_MAX if sign == 1 else INT_MIN
-            num = num * 10 + digit
-            i+=1
-        return sign * num
+#         while i<n and '0' <= s[i] <= '9':
+#             digit = ord(s[i]) - ord('0')
+#             if num > (INT_MAX - digit) // 10:
+#                 return INT_MAX if sign == 1 else INT_MIN
+#             num = num * 10 + digit
+#             i+=1
+#         return sign * num
 
 #Day15 
 #User function Template for python3
