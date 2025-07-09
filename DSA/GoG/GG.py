47cd03a16034f139175ce7bbd5106796cc12b381
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-09 22:10:00 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -253,47 +253,47 @@ class Solution:
 #Day18
 #User function Template for python3
 
-class Solution:
-    def search(self, pat, txt):
-        # code here
-        def computeLPS(pattern):
-            lps = [0] * len(pattern)
-            length = 0
-            i = 1
+# class Solution:
+#     def search(self, pat, txt):
+#         # code here
+#         def computeLPS(pattern):
+#             lps = [0] * len(pattern)
+#             length = 0
+#             i = 1
             
-            while i < len(pattern):
-                if pattern[i] == pattern[length]:
-                    length +=1
-                    lps[i] = length
-                    i+=1
+#             while i < len(pattern):
+#                 if pattern[i] == pattern[length]:
+#                     length +=1
+#                     lps[i] = length
+#                     i+=1
                 
-                else:
-                    if length != 0:
-                        length = lps[length - 1]
-                    else:
-                        lps[i] = 0
-                        i+=1
-            return lps
+#                 else:
+#                     if length != 0:
+#                         length = lps[length - 1]
+#                     else:
+#                         lps[i] = 0
+#                         i+=1
+#             return lps
             
-        m,n = len(pat) , len(txt)
-        lps = computeLPS(pat)
-        result = []
+#         m,n = len(pat) , len(txt)
+#         lps = computeLPS(pat)
+#         result = []
         
-        i=j=0
-        while i < n:
-            if pat[j] == txt[i]:
-                i+=1
-                j+=1
-            if j == m:
-                result.append(i-j)
-                j = lps[j-1]
+#         i=j=0
+#         while i < n:
+#             if pat[j] == txt[i]:
+#                 i+=1
+#                 j+=1
+#             if j == m:
+#                 result.append(i-j)
+#                 j = lps[j-1]
                 
-            elif i< n and pat[j] != txt[i]:
-                if j!= 0:
-                    j = lps[j-1]
-                else:
-                    i+=1
-        return result
+#             elif i< n and pat[j] != txt[i]:
+#                 if j!= 0:
+#                     j = lps[j-1]
+#                 else:
+#                     i+=1
+#         return result
 #Day19
 # class Solution:
 #     def minChar(self, s):
