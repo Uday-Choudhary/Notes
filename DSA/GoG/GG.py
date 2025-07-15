0910c99571763eaf2fb62692aa4bb064beba98fe
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-15 21:41:54 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -430,6 +430,30 @@ class Solution:
                 last[1] = max(last[1] , current[1])
             else:
                 merged.append(current)
+
+#Day25
+	class Solution:
+    def insertInterval(self, intervals, newInterval):
+        # Code here
+        result = []
+        i = 0
+        n = len(intervals)
+        
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i+=1
+        
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0] , intervals[i][0])
+            newInterval[1] = max(newInterval[1] , intervals[i][1])
+            i+=1
+        result.append(newInterval)
+        
+        while i < n:
+            result.append(intervals[i])
+            i+=1
+        
+        return result
         
         return merged
 	        
