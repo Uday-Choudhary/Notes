78d352a355edf2b75bdb33149ae41401589c9a45
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-18 22:00:12 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -513,3 +513,42 @@ class Solution:
             if gap == 1:
                 break
             gap = (gap+1) // 2
+
+#Day28
+class Solution:
+    def countFreq(self, arr, target):
+        #code here
+        def first_occurrence(arr,target):
+            low,high = 0 , len(arr)-1
+            result = -1
+            while low<= high:
+                mid = (low+high) // 2
+                if arr[mid] == target:
+                    result = mid
+                    high = mid-1
+                elif arr[mid] < target:
+                    low = mid+1
+                else:
+                    high = mid -1
+            return result
+        
+        def last_occurrence(arr,target):
+            low,high = 0, len(arr)-1
+            result = -1
+            while low <= high:
+                mid = (low+high) // 2
+                if arr[mid] == target:
+                    result = mid
+                    low = mid+1
+                elif arr[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return result 
+        
+        first = first_occurrence(arr,target)
+        if first == -1:
+            return 0
+        
+        last = last_occurrence(arr,target)
+        return last - first + 1
