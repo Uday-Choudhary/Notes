12fca899478869520f8821da8427192ac99006ff
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2025-07-27 23:08:10 +0530

    Update GG.py

--- DSA/GoG/GG.py
+++ DSA/GoG/GG.py
@@ -714,3 +714,35 @@ class Solution:
                 right = mid - 1
         
         return k+left
+
+#Day36
+class Solution:
+    def spirallyTraverse(self, mat):
+        n = len(mat)
+        m = len(mat[0])
+        result = []
+       
+        top,bottom = 0, n-1
+        left,right = 0, m-1
+       
+        while top <= bottom and left <= right:
+            for i in range(left,right+1):
+                result.append(mat[top][i])
+            top+=1
+              
+            for i in range(top,bottom+1,):
+                result.append(mat[i][right])
+            right -=1
+            
+            if top <= bottom:
+                for i in range(right,left-1,-1):
+                    result.append(mat[bottom][i])
+                bottom -= 1
+            
+            if left <= right:
+                for i in range(bottom,top-1,-1):
+                    result.append(mat[i][left])
+                left+=1
+        return result
+            
+       
