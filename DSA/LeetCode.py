0a13d09959af1b6f6453eb1d65f2bcc85beac3bd
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2026-02-06 21:48:44 +0530

    Update LeetCode.py

--- DSA/LeetCode.py
+++ DSA/LeetCode.py
@@ -314,6 +314,25 @@ class Solution(object):
             
             root.val = successor.val
             root.right = self.deleteNode(root.right, successor.val)
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        if not strs:
+            return ""
+
+        for i in range(len(strs[0])):
+            ch = strs[0][i]
+            for s in strs[1:]:
+                if i == len(s) or s[i] != ch:
+                    return strs[0][:i]
+
+        return strs[0]
+
         
         return root
 
