a09c55ce954bf4ad7a3679f0447d80cd10005473
Uday Kumar Choudhary <choudharyuday85@gmail.com>
2026-02-09 22:43:47 +0530

    Update LeetCode.py

--- DSA/LeetCode.py
+++ DSA/LeetCode.py
@@ -333,6 +333,30 @@ class Solution(object):
 
         return strs[0]
 
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in mapping:  
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:  
+                stack.append(char)
+
+        return len(stack) == 0
+
+
         
         return root
 
