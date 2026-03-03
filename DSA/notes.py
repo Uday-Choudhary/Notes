1842482d91340978f73fe9dddbf3f3d5bd3e579f
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-03 10:30:00 +0530

    DSA: Symmetric Tree - 2026-03-03

--- DSA/notes.py
+++ DSA/notes.py
@@ -234,3 +234,20 @@ class Solution:
             a, b = b, a + b
         return b
 
+
+# Symmetric Tree
+# Given the root of a binary tree, check whether it is a mirror of itself.
+class Solution:
+    def isSymmetric(self, root):
+        def isMirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            return (left.val == right.val and
+                    isMirror(left.left, right.right) and
+                    isMirror(left.right, right.left))
+        if not root:
+            return True
+        return isMirror(root.left, root.right)
+
