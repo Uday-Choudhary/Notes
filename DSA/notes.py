9e3a03583cff1517c20ac42846b7bdbf83ef0961
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-10 10:30:00 +0530

    DSA: Invert Binary Tree - 2026-03-10

--- DSA/notes.py
+++ DSA/notes.py
@@ -330,3 +330,15 @@ class Solution:
         depth(root)
         return self.diameter
 
+
+# Invert Binary Tree
+# Given the root of a binary tree, invert the tree and return its root.
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
