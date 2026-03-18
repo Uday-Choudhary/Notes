f583847637180970d9a06a1c3e5a75f33a53e62b
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-18 12:00:00 +0530

    Added DSA question for 2026-03-18

--- DSA/notes.py
+++ DSA/notes.py
@@ -466,3 +466,17 @@ class Solution:
                 water += right_max - height[right]
         return water
 
+
+
+# Merge Intervals
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals.
+class Solution:
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
