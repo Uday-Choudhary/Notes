80605e5f2e0a6ba160b58a4bd3205f25aee82c1d
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-19 12:00:00 +0530

    Added DSA question for 2026-03-19

--- DSA/notes.py
+++ DSA/notes.py
@@ -480,3 +480,21 @@ class Solution:
             else:
                 merged[-1][1] = max(merged[-1][1], interval[1])
         return merged
+
+
+# Insert Interval
+# Insert a new interval into a sorted array of non-overlapping intervals and merge if necessary.
+class Solution:
+    def insert(self, intervals, newInterval):
+        result = []
+        for i, interval in enumerate(intervals):
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif newInterval[1] < interval[0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            else:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+        result.append(newInterval)
+        return result
