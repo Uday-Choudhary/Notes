12eea9dbae70bd5282e7775352b9c4eef78b4fc8
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-21 12:00:00 +0530

    Added DSA question for 2026-03-21

--- DSA/notes.py
+++ DSA/notes.py
@@ -514,3 +514,14 @@ class Solution:
             else:
                 end = intervals[i][1]
         return count
+
+
+# Meeting Rooms
+# Determine if a person could attend all meetings.
+class Solution:
+    def canAttendMeetings(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+        return True
