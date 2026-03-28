59ac56461277cfebcc8114c071b6f42efea4993f
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-28 12:00:00 +0530

    Add DSA Solution: Search in Rotated Sorted Array

--- DSA/notes.py
+++ DSA/notes.py
@@ -589,3 +589,16 @@ class Solution:
             if nums[mid] > nums[right]: left = mid + 1
             else: right = mid
         return nums[left]
+\n\n# Search in Rotated Sorted Array\nclass Solution:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target: return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]: right = mid - 1
+                else: left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]: left = mid + 1
+                else: right = mid - 1
+        return -1
