f5f2c5ee79ceb7ba1019dafae966d4eacbe34106
Uday-Choudhary <choudharyuday85@gmail.com>
2026-03-29 12:00:00 +0530

    Add DSA Solution: Maximum Product Subarray

--- DSA/notes.py
+++ DSA/notes.py
@@ -602,3 +602,13 @@ class Solution:
                 if nums[mid] < target <= nums[right]: left = mid + 1
                 else: right = mid - 1
         return -1
+\n\n# Maximum Product Subarray\nclass Solution:
+    def maxProduct(self, nums):
+        res = max(nums)
+        curMin, curMax = 1, 1
+        for n in nums:
+            tmp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax)
+        return res
