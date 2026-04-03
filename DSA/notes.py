502cbe29add7d13c9ebd3def75225060de9c9c52
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-03 12:00:00 +0530

    Add DSA Solution: House Robber

--- DSA/notes.py
+++ DSA/notes.py
@@ -650,3 +650,11 @@ class Solution:
             dfs(i + 1, cur, total)
         dfs(0, [], 0)
         return res
+\n\n# House Robber\nclass Solution:
+    def rob(self, nums):
+        rob1, rob2 = 0, 0
+        for n in nums:
+             temp = max(n + rob1, rob2)
+             rob1 = rob2
+             rob2 = temp
+        return rob2
