bf8f3e79cc278a2ece14dbaa45678efc1ef534df
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-06 12:00:00 +0530

    Add DSA Solution: Unique Paths

--- DSA/notes.py
+++ DSA/notes.py
@@ -676,3 +676,12 @@ class Solution:
              else: dp[i] = dp[i + 1]
              if (i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in '0123456')): dp[i] += dp[i + 2]
         return dp[0]
+\n\n# Unique Paths\nclass Solution:
+    def uniquePaths(self, m, n):
+        row = [1] * n
+        for i in range(m - 1):
+             newRow = [1] * n
+             for j in range(n - 2, -1, -1):
+                  newRow[j] = newRow[j + 1] + row[j]
+             row = newRow
+        return row[0]
