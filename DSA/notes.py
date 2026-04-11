0c5f26ee57973b260c834de107c63b818f2ac49d
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-11 12:00:00 +0530

    Add DSA Solution: Number of Islands

--- DSA/notes.py
+++ DSA/notes.py
@@ -743,3 +743,21 @@ class Solution:
                   if (r, c) in pac and (r, c) in atl:
                        res.append([r, c])
         return res
+\n\n# Number of Islands\nclass Solution:
+    def numIslands(self, grid):
+        if not grid: return 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+        def dfs(r, c):
+             if (r not in range(rows) or c not in range(cols) or grid[r][c] == '0' or (r,c) in visited): return
+             visited.add((r, c))
+             directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+             for dr, dc in directions:
+                 dfs(r + dr, c + dc)
+        for r in range(rows):
+             for c in range(cols):
+                 if grid[r][c] == '1' and (r, c) not in visited:
+                      dfs(r, c)
+                      islands += 1
+        return islands
