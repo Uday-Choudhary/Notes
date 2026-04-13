880b2828ef1d7a98510454a6d5dae77a108d4f74
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-13 12:00:00 +0530

    Add DSA Solution: Implement Trie (Prefix Tree)

--- DSA/notes.py
+++ DSA/notes.py
@@ -767,3 +767,28 @@ class Solution:
              if p.val > root.val and q.val > root.val: root = root.right
              elif p.val < root.val and q.val < root.val: root = root.left
              else: return root
+\n\n# Implement Trie (Prefix Tree)\nclass TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, word):
+        cur = self.root
+        for c in word:
+             if c not in cur.children: cur.children[c] = TrieNode()
+             cur = cur.children[c]
+        cur.endOfWord = True
+    def search(self, word):
+        cur = self.root
+        for c in word:
+             if c not in cur.children: return False
+             cur = cur.children[c]
+        return cur.endOfWord
+    def startsWith(self, prefix):
+        cur = self.root
+        for c in prefix:
+             if c not in cur.children: return False
+             cur = cur.children[c]
+        return True
