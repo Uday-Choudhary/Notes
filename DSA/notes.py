7374c78e1beb29e7f9deab995268b37b93b8d571
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-14 12:00:00 +0530

    Add DSA Solution: Design Add and Search Words Data Structure

--- DSA/notes.py
+++ DSA/notes.py
@@ -792,3 +792,30 @@ class Trie:
              if c not in cur.children: return False
              cur = cur.children[c]
         return True
+\n\n# Design Add and Search Words Data Structure\nclass TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+    def addWord(self, word):
+        cur = self.root
+        for c in word:
+             if c not in cur.children: cur.children[c] = TrieNode()
+             cur = cur.children[c]
+        cur.word = True
+    def search(self, word):
+        def dfs(j, root):
+             cur = root
+             for i in range(j, len(word)):
+                  c = word[i]
+                  if c == '.':
+                       for child in cur.children.values():
+                            if dfs(i + 1, child): return True
+                       return False
+                  else:
+                       if c not in cur.children: return False
+                       cur = cur.children[c]
+             return cur.word
+        return dfs(0, self.root)
