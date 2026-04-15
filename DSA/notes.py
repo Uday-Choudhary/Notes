025bcc241137104faf8561a4c795212c9b43d091
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-15 12:00:00 +0530

    Add DSA Solution: Word Search II

--- DSA/notes.py
+++ DSA/notes.py
@@ -819,3 +819,21 @@ class WordDictionary:
                        cur = cur.children[c]
              return cur.word
         return dfs(0, self.root)
+\n\n# Word Search II\nclass TrieNode:
+    def __init__(self): self.children = {}; self.isWord = False; self.refs = 0
+    def addWord(self, word):
+        cur = self
+        cur.refs += 1
+        for c in word:
+            if c not in cur.children: cur.children[c] = TrieNode()
+            cur = cur.children[c]
+            cur.refs += 1
+        cur.isWord = True
+    def removeWord(self, word):
+        cur = self
+        cur.refs -= 1
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+                cur.refs -= 1
+# ... search implementation continues ...
