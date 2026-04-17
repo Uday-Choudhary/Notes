60fb8aa7e6196d57ca16c3f797fde2da8d0c6ea0
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-17 12:00:00 +0530

    Add DSA Solution: Find Median from Data Stream

--- DSA/notes.py
+++ DSA/notes.py
@@ -849,3 +849,20 @@ class WordDictionary:
              k -= 1
              if k == 0: return curr.val
              curr = curr.right
+\n\n# Find Median from Data Stream\nimport heapq
+class MedianFinder:
+    def __init__(self):
+        self.small, self.large = [], []
+    def addNum(self, num):
+        if self.large and num > self.large[0]: heapq.heappush(self.large, num)
+        else: heapq.heappush(self.small, -1 * num)
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+    def findMedian(self):
+        if len(self.small) > len(self.large): return -1 * self.small[0]
+        elif len(self.large) > len(self.small): return self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2.0
