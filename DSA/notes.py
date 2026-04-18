6d6a0a7cd99be968d627e829429f65bb49673ca5
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-18 12:00:00 +0530

    Add DSA Solution: Longest Repeating Character Replacement

--- DSA/notes.py
+++ DSA/notes.py
@@ -866,3 +866,17 @@ class MedianFinder:
         if len(self.small) > len(self.large): return -1 * self.small[0]
         elif len(self.large) > len(self.small): return self.large[0]
         return (-1 * self.small[0] + self.large[0]) / 2.0
+\n\n# Longest Repeating Character Replacement\nclass Solution:
+    def characterReplacement(self, s, k):
+        count = {}
+        res = 0
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+            if (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
